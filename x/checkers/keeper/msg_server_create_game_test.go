853b6b6d08c57fca7917246486213e03f91843f5
jrstrunk <developer@jrstrunk.com>
2022-06-15 19:23:16 -0400

    output from command: ignite scaffold message playMove idValue fromX:uint fromY:uint toX:uint toY:uint --module checkers --response idValue

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -38,27 +38,27 @@ func TestCreateGame(t *testing.T) {
 }
 
 func TestCreate1GameHasSaved(t *testing.T) {
-    msgSrvr, keeper, context := setupMsgServerCreateGame(t)
-    msgSrvr.CreateGame(context, &types.MsgCreateGame{
-        Creator: alice,
-        Red:     bob,
-        Black:   carol,
-    })
-    nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
-    require.True(t, found)
-    require.EqualValues(t, types.NextGame{
-        IdValue: 2,
-    }, nextGame)
-    game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
-    require.True(t, found1)
-    require.EqualValues(t, types.StoredGame{
-        Creator: alice,
-        Index:   "1",
-        Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-        Turn:    "b",
-        Red:     bob,
-        Black:   carol,
-    }, game1)
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		IdValue: 2,
+	}, nextGame)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, game1)
 }
 
 func TestCreate1GameGetAll(t *testing.T) {
