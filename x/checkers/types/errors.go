f0d1cf793aefdd2769809b2af672706f6b3010ee
jrstrunk <developer@jrstrunk.com>
2022-06-15 20:35:40 -0400

    added sdk error definitions for moving a piece on the board

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -13,4 +13,8 @@ var (
 	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1102, "black address is invalid: %s")
 	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1103, "game cannot be parsed")
 	ErrInvalidIndex     = sdkerrors.Register(ModuleName, 1104, "game index is invalid: %s")
+	ErrGameNotFound     = sdkerrors.Register(ModuleName, 1105, "game by id not found: %s")
+	ErrCreatorNotPlayer = sdkerrors.Register(ModuleName, 1106, "message creator is not a player: %s")
+	ErrNotPlayerTurn    = sdkerrors.Register(ModuleName, 1107, "player tried to play out of turn: %s")
+	ErrWrongMove        = sdkerrors.Register(ModuleName, 1108, "wrong move")
 )
