4b534b7cdc738c4f430773b171a99b9300a7ef06
jrstrunk <developer@jrstrunk.com>
2022-06-15 21:21:08 -0400

    added module handling for a move message

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -2,16 +2,72 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// get the stored game from memory
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.IdValue)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "game not found %s", msg.IdValue)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	// determine which player the message creator is
+	isRed := strings.Compare(storedGame.Red, msg.Creator) == 0
+	isBlack := strings.Compare(storedGame.Black, msg.Creator) == 0
+	var player rules.Player
+	if !isRed && !isBlack {
+		return nil, types.ErrCreatorNotPlayer
+	} else if isRed && isBlack {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isRed {
+		player = rules.RED_PLAYER
+	} else {
+		player = rules.BLACK_PLAYER
+	}
+
+	// parse game state
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// if it is not the current player's term, they cannot move so return an error
+	if !game.TurnIs(player) {
+		return nil, types.ErrNotPlayerTurn
+	}
+
+	// make the move
+	captured, moveErr := game.Move(
+		rules.Pos{
+			X: int(msg.FromX),
+			Y: int(msg.FromY),
+		},
+		rules.Pos{
+			X: int(msg.ToX),
+			Y: int(msg.ToY),
+		},
+	)
+	if moveErr != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, moveErr.Error())
+	}
+
+	// save the modified game state
+	storedGame.Game = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	return &types.MsgPlayMoveResponse{
+		IdValue:   msg.IdValue,
+		CapturedX: int64(captured.X),
+		CapturedY: int64(captured.Y),
+		Winner:    game.Winner().Color,
+	}, nil
 }
