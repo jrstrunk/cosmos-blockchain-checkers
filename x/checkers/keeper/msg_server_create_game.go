80d1df10906470420a5d22c23b91e4e75f995a23
jrstrunk <developer@jrstrunk.com>
2022-06-18 09:55:55 -0400

    added an event emission when a player startes a new game containing game details

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -51,6 +51,18 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 	nextGame.IdValue++
 	k.Keeper.SetNextGame(ctx, nextGame)
 
+	// emit an event to notify the player a game has been started
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, "checkers"),
+			sdk.NewAttribute(sdk.AttributeKeyAction, types.StoredGameEventKey),
+			sdk.NewAttribute(types.StoredGameEventCreator, msg.Creator),
+			sdk.NewAttribute(types.StoredGameEventIndex, newIndex),
+			sdk.NewAttribute(types.StoredGameEventRed, msg.Red),
+			sdk.NewAttribute(types.StoredGameEventBlack, msg.Black),
+		),
+	)
+
 	// return the IdValue for the newly created game
 	return &types.MsgCreateGameResponse{
 		IdValue: newIndex,
