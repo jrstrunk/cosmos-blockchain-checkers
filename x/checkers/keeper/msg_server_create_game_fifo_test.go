dd93359068ebb4f1c87524ea7cb994eabe56fd0c
jrstrunk <developer@jrstrunk.com>
2022-06-20 17:03:52 -0400

    adjusted unit tests for the deadline field in a stored game

--- x/checkers/keeper/msg_server_create_game_fifo_test.go
+++ x/checkers/keeper/msg_server_create_game_fifo_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestCreate3GamesHasSavedFifo(t *testing.T) {
 	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Red:     bob,
@@ -40,6 +41,7 @@ func TestCreate3GamesHasSavedFifo(t *testing.T) {
 		MoveCount: uint64(0),
 		BeforeId:  "-1",
 		AfterId:   "2",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
 	}, game1)
 	game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
 	require.True(t, found2)
@@ -53,6 +55,7 @@ func TestCreate3GamesHasSavedFifo(t *testing.T) {
 		MoveCount: uint64(0),
 		BeforeId:  "1",
 		AfterId:   "-1",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
 	}, game2)
 
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
@@ -79,6 +82,7 @@ func TestCreate3GamesHasSavedFifo(t *testing.T) {
 		MoveCount: uint64(0),
 		BeforeId:  "-1",
 		AfterId:   "2",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
 	}, game1)
 	game2, found2 = keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
 	require.True(t, found2)
@@ -92,6 +96,7 @@ func TestCreate3GamesHasSavedFifo(t *testing.T) {
 		MoveCount: uint64(0),
 		BeforeId:  "1",
 		AfterId:   "3",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
 	}, game2)
 	game3, found3 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "3")
 	require.True(t, found3)
@@ -105,199 +110,6 @@ func TestCreate3GamesHasSavedFifo(t *testing.T) {
 		MoveCount: uint64(0),
 		BeforeId:  "2",
 		AfterId:   "-1",
-	}, game3)
-}
-
-func TestPlayMove2Games1MoveHasSavedFifo(t *testing.T) {
-	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
-	msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: bob,
-		Red:     carol,
-		Black:   alice,
-	})
-
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator: carol,
-		IdValue: "1",
-		FromX:   1,
-		FromY:   2,
-		ToX:     2,
-		ToY:     3,
-	})
-	nextGame1, found1 := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
-	require.True(t, found1)
-	require.EqualValues(t, types.NextGame{
-		IdValue:  3,
-		FifoHead: "2",
-		FifoTail: "1",
-	}, nextGame1)
-	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
-	require.True(t, found1)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   alice,
-		Index:     "1",
-		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "r",
-		Red:       bob,
-		Black:     carol,
-		MoveCount: uint64(1),
-		BeforeId:  "2",
-		AfterId:   "-1",
-	}, game1)
-	game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
-	require.True(t, found2)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   bob,
-		Index:     "2",
-		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Red:       carol,
-		Black:     alice,
-		MoveCount: uint64(0),
-		BeforeId:  "-1",
-		AfterId:   "1",
-	}, game2)
-}
-
-func TestPlayMove2Games2MovesHasSavedFifo(t *testing.T) {
-	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
-	msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: bob,
-		Red:     carol,
-		Black:   alice,
-	})
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator: carol,
-		IdValue: "1",
-		FromX:   1,
-		FromY:   2,
-		ToX:     2,
-		ToY:     3,
-	})
-
-	msgServer.PlayMove(context, &types.MsgPlayMove{
-		Creator: alice,
-		IdValue: "2",
-		FromX:   1,
-		FromY:   2,
-		ToX:     2,
-		ToY:     3,
-	})
-	nextGame1, found1 := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
-	require.True(t, found1)
-	require.EqualValues(t, types.NextGame{
-		IdValue:  3,
-		FifoHead: "1",
-		FifoTail: "2",
-	}, nextGame1)
-	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
-	require.True(t, found1)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   alice,
-		Index:     "1",
-		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "r",
-		Red:       bob,
-		Black:     carol,
-		MoveCount: uint64(1),
-		BeforeId:  "-1",
-		AfterId:   "2",
-	}, game1)
-	game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
-	require.True(t, found2)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   bob,
-		Index:     "2",
-		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "r",
-		Red:       carol,
-		Black:     alice,
-		MoveCount: uint64(1),
-		BeforeId:  "1",
-		AfterId:   "-1",
-	}, game2)
-}
-
-func TestRejectSecondGameHasSavedFifo(t *testing.T) {
-	msgServer, keeper, context := setupMsgServerWithOneGameForRejectGame(t)
-	msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: bob,
-		Red:     carol,
-		Black:   alice,
-	})
-	msgServer.RejectGame(context, &types.MsgRejectGame{
-		Creator: carol,
-		IdValue: "1",
-	})
-	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
-	require.True(t, found)
-	require.EqualValues(t, types.NextGame{
-		IdValue:  3,
-		FifoHead: "2",
-		FifoTail: "2",
-	}, nextGame)
-	game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
-	require.True(t, found2)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   bob,
-		Index:     "2",
-		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Red:       carol,
-		Black:     alice,
-		MoveCount: uint64(0),
-		BeforeId:  "-1",
-		AfterId:   "-1",
-	}, game2)
-}
-
-func TestRejectMiddleGameHasSavedFifo(t *testing.T) {
-	msgServer, keeper, context := setupMsgServerWithOneGameForRejectGame(t)
-	msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: bob,
-		Red:     carol,
-		Black:   alice,
-	})
-	msgServer.CreateGame(context, &types.MsgCreateGame{
-		Creator: carol,
-		Red:     alice,
-		Black:   bob,
-	})
-	msgServer.RejectGame(context, &types.MsgRejectGame{
-		Creator: carol,
-		IdValue: "2",
-	})
-	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
-	require.True(t, found)
-	require.EqualValues(t, types.NextGame{
-		IdValue:  4,
-		FifoHead: "1",
-		FifoTail: "3",
-	}, nextGame)
-	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
-	require.True(t, found1)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   alice,
-		Index:     "1",
-		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Red:       bob,
-		Black:     carol,
-		MoveCount: uint64(0),
-		BeforeId:  "-1",
-		AfterId:   "3",
-	}, game1)
-	game3, found3 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "3")
-	require.True(t, found3)
-	require.EqualValues(t, types.StoredGame{
-		Creator:   carol,
-		Index:     "3",
-		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:      "b",
-		Red:       alice,
-		Black:     bob,
-		MoveCount: uint64(0),
-		BeforeId:  "1",
-		AfterId:   "-1",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
 	}, game3)
 }
---
+++ x/checkers/keeper/msg_server_reject_game_fifo_test.go
@@ -0,0 +1,98 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRejectSecondGameHasSavedFifo(t *testing.T) {
+	msgServer, keeper, context := setupMsgServerWithOneGameForRejectGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Red:     carol,
+		Black:   alice,
+	})
+	msgServer.RejectGame(context, &types.MsgRejectGame{
+		Creator: carol,
+		IdValue: "1",
+	})
+	nextGame, found := keeper.GetNextGame(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		IdValue:  3,
+		FifoHead: "2",
+		FifoTail: "2",
+	}, nextGame)
+	game2, found2 := keeper.GetStoredGame(ctx, "2")
+	require.True(t, found2)
+	require.EqualValues(t, types.StoredGame{
+		Creator:   bob,
+		Index:     "2",
+		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Red:       carol,
+		Black:     alice,
+		MoveCount: uint64(0),
+		BeforeId:  "-1",
+		AfterId:   "-1",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+	}, game2)
+}
+
+func TestRejectMiddleGameHasSavedFifo(t *testing.T) {
+	msgServer, keeper, context := setupMsgServerWithOneGameForRejectGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Red:     carol,
+		Black:   alice,
+	})
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Red:     alice,
+		Black:   bob,
+	})
+	msgServer.RejectGame(context, &types.MsgRejectGame{
+		Creator: carol,
+		IdValue: "2",
+	})
+	nextGame, found := keeper.GetNextGame(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		IdValue:  4,
+		FifoHead: "1",
+		FifoTail: "3",
+	}, nextGame)
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Creator:   alice,
+		Index:     "1",
+		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Red:       bob,
+		Black:     carol,
+		MoveCount: uint64(0),
+		BeforeId:  "-1",
+		AfterId:   "3",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+	}, game1)
+	game3, found3 := keeper.GetStoredGame(ctx, "3")
+	require.True(t, found3)
+	require.EqualValues(t, types.StoredGame{
+		Creator:   carol,
+		Index:     "3",
+		Game:      "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Red:       alice,
+		Black:     bob,
+		MoveCount: uint64(0),
+		BeforeId:  "1",
+		AfterId:   "-1",
+		Deadline:  types.FormatDeadline(ctx.BlockTime().Add(types.MaxTurnDuration)),
+	}, game3)
+}
